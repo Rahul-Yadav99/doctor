5515e6c10cabde8317eee26394a839884d04edc1
rahul <rahul352sj@gmail.com>
2024-06-16 13:45:39 +0530

    minor changes

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import 'remixicon/fonts/remixicon.css'
+import 'animate.css'
+import {
+    BrowserRouter,
+    Routes,
+    Route
+} from "react-router-dom";
+import Home from "./components/Home.jsx"
+const app = ()=>{
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path="/home" element={<Home/>}/>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+export default app
